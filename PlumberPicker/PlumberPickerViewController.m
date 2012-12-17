b2be32e233e39ca4db24a7bfb465d09573f0e719
simon olesen <srolesen@gmail.com>
2012-12-17 01:17:54 +0100

    settings now in plist file, so use settings app to set local prices/use

--- PlumberPicker/PlumberPickerViewController.m
+++ PlumberPicker/PlumberPickerViewController.m
@@ -21,6 +21,20 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+        //init of the root.plist settings
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        NSString *settingsBundle =
+        [[NSBundle mainBundle] pathForResource:@"Settings" ofType:@"bundle"];
+        NSDictionary *settings =
+        [NSDictionary dictionaryWithContentsOfFile:
+         [settingsBundle stringByAppendingPathComponent:@"Root.plist"]];
+        
+        NSArray *preferences = [settings objectForKey:@"PreferenceSpecifiers"];
+        NSMutableDictionary *defaultsToRegister =
+        [[NSMutableDictionary alloc] initWithCapacity:[preferences count]];
+        
+        [defaults registerDefaults:defaultsToRegister];
+        //end "init of the root.plist settings"
 }
 
 - (void)didReceiveMemoryWarning
@@ -125,6 +139,13 @@
     float tapConsumption = ((float)[_pickerView selectedRowInComponent:3]/5);
         //    int mainsPressure = [_pickerView selectedRowInComponent:2];
     
+    // set variables here from Root.strings
+    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+    float wateruse =(float)([prefs floatForKey:@"wateruse_day"]);
+    float waterprice = ([prefs floatForKey:@"m3_waterprice"]);
+    NSLog(@"how many times a day the tap is turned on %f", wateruse);
+    NSLog(@"how much does water cost %f", waterprice);
+
     
     //Let's print in the console what the user had chosen;
     NSLog(@"rørdiameter i cm %f",pipeWidth);
@@ -141,9 +162,9 @@
     self.label.text = myNewString;
     NSString* myWaterString = [NSString stringWithFormat:@"%0.1f l", pipeVolume];
     self.wWater.text = myWaterString;
-    NSString* myWaterYearString = [NSString stringWithFormat:@"%0.1f l", pipeVolume*8*365];
+    NSString* myWaterYearString = [NSString stringWithFormat:@"%0.1f l", pipeVolume*wateruse*365];
     self.wYear.text = myWaterYearString;
-    NSString* myPriceYear = [NSString stringWithFormat:@"%0.1f kr", pipeVolume*8*365*0.25];
+    NSString* myPriceYear = [NSString stringWithFormat:@"%0.1f kr", pipeVolume*wateruse*365*waterprice*0.001];
     self.pYear.text = myPriceYear;
 }
 @end
